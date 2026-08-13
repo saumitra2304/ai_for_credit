5c7d064e50ffd46e04a5c07fc0891e4cc2e72028
suparn <suparnkarmakar01@gmail.com>
2026-08-13 14:23:28 +0530

    probe integration done

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use reqwest::Client;
 use std::sync::Arc;
 mod routes;
 use routes::finacial_summary;
+use routes::probe_router::{probe_search,company_comprehensive_details};
+
+use std::env;
 
 use crate::routes::finacial_summary::{brisk_all, insta_summary};
 
@@ -15,6 +18,7 @@ use crate::routes::finacial_summary::{brisk_all, insta_summary};
 struct AppState {
     api_key: String,
     reqwest_client:Client,
+    probe_key:String,
    
 }
 
@@ -23,11 +27,14 @@ async fn main() {
 
     let client=reqwest::Client::new();
 
+    let probe_key=extract_en();
+
 
     // 2. Instantiate your state wrapped in Arc
     let state = AppState {
         api_key: String::from("koHNEOKWX6/1KMOIiUDMzrWAxS9rvh8rHvzZtQWcfj+BTb5vA0lSiw=="),
-        reqwest_client:client
+        reqwest_client:client,
+        probe_key:probe_key
         
     };
 
@@ -35,13 +42,27 @@ async fn main() {
     let app = Router::new()
         .route("/insta_summary", get(insta_summary))
         .with_state(state.clone())
-        .route("/brisk_all", get(brisk_all)).with_state(state.clone());
+        .route("/brisk_all", get(brisk_all)).with_state(state.clone())
+        .route("/search_company", get(probe_search)).with_state(state.clone())
+        .route("/company_details",get(company_comprehensive_details).with_state(state.clone()));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
     
     axum::serve(listener, app).await.unwrap();
+}
+
+fn extract_en()->String{
+    dotenvy::dotenv().ok();
+
+    // 2. Read environment variables
+    
+    // Method A: Using unwrap or expect (panics if variable isn't set)
+    let probe_key = env::var("probe_api_key")
+        .expect("probe_api_key must be set in .env or environment");
+
+    return probe_key;
 }
 
 // 4. Extract state in your handler
--- src/routes.rs
+++ src/routes.rs
@@ -1,3 +1,5 @@
 pub mod finacial_summary;
 
 pub mod models;
+pub mod probe_router;
+pub mod probe_models;
